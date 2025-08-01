5dc9f2da3b3f1672b64c9df26a1d72e6585664e1
a.kenawy@v4tech.com <a.kenawy@v4tech.com>
2025-08-01 20:28:04 +0300

    adding the proof of work

---
+++ Proof of Work/app.js
@@ -0,0 +1,45 @@
+const SHA256 = require('crypto-js/sha256');
+const TARGET_DIFFICULTY = BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+const MAX_TRANSACTIONS = 10;
+const mempool = [];
+const blocks = [];
+
+function addTransaction(transaction) {
+    mempool.push(transaction);
+}
+
+function mine() {
+    let transactions = [];
+    while (transactions.length < MAX_TRANSACTIONS && mempool.length > 0) {
+        transactions.push(mempool.pop());
+    }
+    const block = { id: blocks.length, transactions }
+    block.nonce = 0;
+    let hash;
+    while (true) {
+        hash = SHA256(JSON.stringify(block)).toString();
+        if (BigInt(`0x${hash}`) < TARGET_DIFFICULTY) {
+            break;
+        }
+        block.nonce++;
+    }
+    blocks.push({ ...block, hash });
+}
+
+if (require.main === module) {
+    addTransaction({ from: "Alice", to: "Bob", amount: 10 });
+    addTransaction({ from: "Charlie", to: "Dave", amount: 20 });
+    console.log("Mempool before mining:", mempool);
+    mine();
+    console.log("Blocks after mining:", blocks);
+}
+
+
+module.exports = {
+    TARGET_DIFFICULTY,
+    MAX_TRANSACTIONS,
+    addTransaction,
+    mine,
+    mempool,
+    blocks,
+};
