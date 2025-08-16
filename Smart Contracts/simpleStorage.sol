908fd8aa70d0acf760d0a1ba7e794d24cdc67c8b
Ali-Adel-Nour <aliiiadel841@gmail.com>
2025-08-16 19:20:48 +0300

    Testing list type in solidity

--- Smart Contracts/simpleStorage.sol
+++ Smart Contracts/simpleStorage.sol
@@ -2,16 +2,33 @@ pragma solidity 0.8.19;
 
 contract SimpleStroage  {
 
-    uint256 public favoriteNumber; //default 0
+    uint256 public myfavoriteNumber; //default 0
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+ Person [] public listofPeoples; 
+
+ function addPerson(string memory name, uint256 _favoriteNumber) public {
+
+   listofPeoples.push(Person({favoriteNumber: _favoriteNumber, name: name}));
+
+ }
+
+   // Person public pat  = Person(2, "Pat"); // good 
+
+  //  Person public pat  = Person({favoriteNumber:7, name: "Pat"}); // Best Practice
 
     function store (uint256 _favoriteNumber) public {
-      favoriteNumber = _favoriteNumber;
+      myfavoriteNumber = _favoriteNumber;
     }
 
 //there is two types view-> this for return the state without editing it
 // pure adding the state like the store
    function retreive () public view returns (uint256) {
-        return favoriteNumber;
+        return myfavoriteNumber;
     }
 
 }
