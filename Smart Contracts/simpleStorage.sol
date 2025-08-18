e562a5b4469dd749d8e0ab1f598683acc9f8dcd5
Ali-Adel-Nour <aliiiadel841@gmail.com>
2025-08-18 20:18:20 +0300

    Mapping in solidity

--- Smart Contracts/simpleStorage.sol
+++ Smart Contracts/simpleStorage.sol
@@ -1,4 +1,6 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
+
 
 contract SimpleStroage  {
 
@@ -11,10 +13,12 @@ contract SimpleStroage  {
 
  Person [] public listofPeoples; 
 
+ mapping (string => uint256) public nameToFavoriteNumber;
+
  function addPerson(string memory name, uint256 _favoriteNumber) public {
 
    listofPeoples.push(Person({favoriteNumber: _favoriteNumber, name: name}));
-
+   nameToFavoriteNumber[name] = _favoriteNumber;
  }
 
    // Person public pat  = Person(2, "Pat"); // good 
