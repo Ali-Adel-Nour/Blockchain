d6ace77427871c685017fdf4b4fb33cf469c2e47
Ali-Adel-Nour <aliiiadel841@gmail.com>
2025-08-22 16:43:41 +0300

    Adding chainlink

---
+++ Smart Contracts/Fundme/FundMe.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+
+contract FundMe{
+function  fund() public payable {
+
+  require(msg.value > 1e18,"didnt put a eth");  //1e18 = 1ETH
+}
+
+   function getPrice() public view returns (uint256) {
+        // Sepolia ETH / USD Address
+        // https://docs.chain.link/data-feeds/price-feeds/addresses
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        // ETH/USD rate in 18 digit
+        return  uint256(answer) ;
+   }
+// function convertionRate()public view {
+
+// }
+}
